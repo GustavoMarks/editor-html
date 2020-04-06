4d861c6c00b63ae534c31c2fc6a73b1b7b74ffe7
GustavoM7 <gustavosmarquesf@gmail.com>
2020-04-06 19:59:23 -0300

    iniciando e estruturando projeto react

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Editor from './Editor';
+import './App.css';
+
+function App() {
+  return (
+    <Editor/>
+  );
+}
+
+export default App;
---
+++ app/src/Editor/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+export default function Editor() {
+  return(
+    <h1>Hello World!</h1>
+  )
+} 
